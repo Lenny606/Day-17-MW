408aefcd2cc90857bbbc026798645a632ad4ef42
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-05-26 15:02:52 +0200

    progres with the button

--- src/js/ProgressBar.js
+++ src/js/ProgressBar.js
@@ -1,10 +1,12 @@
 export class ProgressBar {
-  constructor(value, gradient, maxValue) 
+  constructor(value, gradient, maxValue, color, globalObject)
   //implement default value + maxvalue
   {
     this.value = value;
     this.gradient = gradient;
     this.maxValue = maxValue;
+    this.color = color;
+    this.globalObject = globalObject;
     //@TODO no.4 make it possible to create progress bar with any default value and gradient. these values should be passed to constructor
     //@TODO no.5 make it possible to create progress bar with any maximum value. once using maximum value, make sure the width of knob element is calculated correctly
 
@@ -13,6 +15,7 @@ export class ProgressBar {
   }
 
   refreshElement() {
+
     this.element.innerHTML = `
       <div class="label">
         ${this.value}/${this.maxValue} 
@@ -20,16 +23,24 @@ export class ProgressBar {
       <div class="progress">
          <div class="btn-minus"></div>
          <div class="bar">
-           <div style="width: ${this.value / this.maxValue * 100}%" class="knob"></div>
+           <div style="width: ${this.value / this.maxValue * 100}%; background-color: ${this.color}" class="knob"></div>
          </div>
          <div class="btn-plus"></div>
       </div>
+      
     `;
+
+
+
 
     const plusBtn = this.element.querySelector('.btn-plus');
     plusBtn.addEventListener('click', () => {
       this.value = Math.min(this.maxValue, this.value + this.gradient);
       this.refreshElement();
+      // this.globalObject.red = this.value
+      // this.globalObject.green = this.value
+      // this.globalObject.blue = this.value
+      // console.log(this.globalObject)
     });
 
     const minusBtn = this.element.querySelector('.btn-minus');
--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,40 @@
 import { ProgressBar } from './ProgressBar';
-
+const target = document.querySelector("body")
+const bars = []
+const globalObject = {
+  red: 5,
+  blue:10,
+  green: 25
+}
 document.addEventListener('DOMContentLoaded', () => {
   // @TODO no.1 create new instance of ProgressBar and append it body element.
   //    you shouldn't pass the whole progress bar to appendChild method, but only specific property
   //    explore class ProgressBar and find out which property it is.
-      const target = document.querySelector("body")
-      
-      const bar = new ProgressBar(5,5,20)
-      target.appendChild(bar.element)
-      
-      const bar2 = new ProgressBar(10,25,100)
-      target.appendChild(bar2.element)
 
-      const bar3 = new ProgressBar(1,1,1)
-      target.appendChild(bar3.element)
-     
-      
+
+  bars.push(new ProgressBar(5, 10, 255, "red"))
+  target.appendChild(bars[bars.length - 1].element)
+
+
+  bars.push(new ProgressBar(10, 10, 255, "green"))
+  target.appendChild((bars[bars.length - 1].element))
+
+  bars.push(new ProgressBar(5,20,255, "blue"))
+  target.appendChild((bars[bars.length-1].element))
+  // const bar3 = new ProgressBar(1, 10, 255, "blue")
+  // target.appendChild(bar3.element)
+
+
 
   // @TODO no.2 fix missing icon on plus button
 });
+
+const btn = document.createElement("button")
+btn.textContent = "CLICK ON ME TO CHANGE MY BACKGROUND"
+btn.addEventListener('click', () => {
+  console.log(bars[0].value, bars[1].value, bars[2].value)
+  btn.style.backgroundColor = `rgb(${bars[0].value},${bars[1].value},${bars[2].value})`
+})
+target.appendChild(btn)
+
+
